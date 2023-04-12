0da9710c6fad8f3c6b009d2984135677408325eb
Mirosław Stanek <froger.mcs@gmail.com>
2023-04-12 16:29:44 +0200

    Init commit

---
+++ functions/src/index.ts
@@ -0,0 +1,82 @@
+import * as functions from "firebase-functions";
+import { IsNotEmpty, Min, validateOrReject } from "class-validator";
+import { RequestHelper } from "./request-helper";
+import { BigQuery } from "@google-cloud/bigquery";
+
+export class Progress {
+  @IsNotEmpty() project: string;
+  @IsNotEmpty() progress: number;
+  @Min(1) timestamp: number;
+
+  constructor(
+      progress: Progress = {
+          project: "",
+          progress: 0,
+          timestamp: 0,
+      }) {
+      this.project = progress.project;
+      this.progress = progress.progress;
+      this.timestamp = progress.timestamp;
+  }
+}
+
+export const reportProgress = functions.https.onRequest(
+    async (request, response) => {
+        const requestHelper: RequestHelper = new RequestHelper();
+        requestHelper.logRequestDetails(request);
+        if (requestHelper.isPostMethod(request)) {
+            try {
+                const progress = await parseAndValidateRequest(request);
+                await saveProgressToDatabase(progress);
+                response.status(200).send(request.body);
+            } catch (error) {
+                requestHelper.respondWithError(response, 400, error);
+            }
+        } else {
+            requestHelper.respondWithError(response, 403, "Forbidden!");
+        }
+    });
+
+async function parseAndValidateRequest(request: functions.https.Request) {
+    const progress = new Progress(request.body);
+    await validateOrReject(progress, { validationError: { target: false } });
+    return progress;
+}
+
+async function saveProgressToDatabase(progress: Progress) {
+    const bq = new BigQuery();
+    const datasetName = "reporting_dataset";
+    const tableName = "datapoints";
+    const dataset = bq.dataset(datasetName);
+    const table = dataset.table(tableName);
+
+    // DB Schema
+    // project STRING REQUIRED
+    // timestamp INTEGER REQUIRED
+    // progress FLOAT REQUIRED
+
+    const tableSchema = [
+        { name: "project", type: "STRING", mode: "REQUIRED" },
+        { name: "timestamp", type: "INTEGER", mode: "REQUIRED" },
+        { name: "progress", type: "FLOAT", mode: "REQUIRED" },
+    ];
+
+    const datasetExists = await dataset.exists();
+    if (!datasetExists[0]) {
+        await dataset.create();
+    }
+
+    const tableExists = await table.exists();
+    if (!tableExists[0]) {
+        const options = { schema: tableSchema };
+        await table.create(options);
+    }
+
+    const row = {
+        project: progress.project,
+        progress: progress.progress,
+        timestamp: progress.timestamp,
+    };
+
+    await table.insert(row);
+}
---
+++ functions/src/request-helper.ts
@@ -0,0 +1,39 @@
+import * as functions from "firebase-functions";
+
+/**
+ * @fileoverview Helper class for handling requests.
+ */
+export class RequestHelper {
+    /**
+     * Logs the request details.
+     * @param {functions.https.Request} request The request to handle.
+     */
+    logRequestDetails(request: functions.https.Request) {
+        console.log("Request headers: " + JSON.stringify(request.headers));
+        console.log("Request body: " + JSON.stringify(request.body));
+    }
+
+    /**
+     * Checks if the request is a POST request.
+     * @param {functions.https.Request} request The request to handle.
+     * @return {boolean} True if the request is a POST request, false otherwise.
+     */
+    isPostMethod(request: functions.https.Request) {
+        return request.method === "POST";
+    }
+
+    /**
+     * Respond to request with given status and errors.
+     * @param {functions.Response} response
+     * @param {number} status
+     * @param {unknown} errors
+     */
+    respondWithError(response: functions.Response,
+        status: number,
+        errors: unknown) {
+        response
+            .set("Content-Type", "application/json")
+            .status(status)
+            .send({ error: errors });
+    }
+}
